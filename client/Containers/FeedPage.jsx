54c06b22de80cce9ae5ebcd0f72516a7705b4ebc
cyduckk <cyrux@live.com>
2023-07-26 11:10:44 -0700

    adds query functionality to refresh button

--- client/Containers/FeedPage.jsx
+++ client/Containers/FeedPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { EditIcon, SunIcon } from '@chakra-ui/icons';
+import React, { useEffect } from 'react';
+import { EditIcon, RepeatIcon } from '@chakra-ui/icons';
 import Post from '../Components/Post';
 import { useRef, useState } from 'react';
 import Languages from '../Components/Languages.jsx';
@@ -29,7 +29,6 @@ import {
 import { Stack, HStack, VStack } from '@chakra-ui/react';
 import { Checkbox, CheckboxGroup } from '@chakra-ui/react';
 
-
 const Feed = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const initialRef = useRef(null);
@@ -41,6 +40,24 @@ const Feed = () => {
   const salaryRef = useRef('');
 
   const [languages, setLanguages] = useState(new Set());
+  const [post, setPost] = useState([]);
+  const handleRefresh = async (e) => {
+    try {
+      const response = await fetch('/posts');
+      if (!response.ok) {
+        throw new Error('Failed to fetch posts');
+      }
+      const data = await response.json();
+      setPost(data);
+      return <Post></Post>;
+    } catch (error) {
+      console.log('Error fetching posts: ', error);
+    }
+  };
+
+  useEffect(() => {
+    handleRefresh();
+  }, []);
 
   const handleNewPost = async (e) => {
     e.preventDefault();
@@ -73,17 +90,16 @@ const Feed = () => {
 
   const getLanguages = (e) => {
     e.preventDefault();
-    
+
     const value = e.target.value;
     const isChecked = e.target.checked; // --> true or false
     console.log('isChecked:', isChecked);
     console.log('getLanguages synthetic event:', e.target.value);
-    
+
     const copy = structuredClone(languages);
 
     if (isChecked) copy.add(value);
     else copy.delete(value);
-
 
     setLanguages(copy);
   };
@@ -92,53 +108,55 @@ const Feed = () => {
     <div>
       <div className='nav'>
         <div className='home-button'>
-          <Button leftIcon={<SunIcon />}>Home</Button>
+          <Button onClick={handleRefresh} leftIcon={<RepeatIcon />}></Button>
         </div>
         <div className='post-button'>
-          <Button onClick={onOpen} leftIcon={<EditIcon />}>Add Post</Button>
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create New Post</ModalHeader>
-          <ModalCloseButton />
+          <Button onClick={onOpen} leftIcon={<EditIcon />}>
+            Add Post
+          </Button>
+          <Modal
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+            isOpen={isOpen}
+            onClose={onClose}
+          >
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Create New Post</ModalHeader>
+              <ModalCloseButton />
 
-          <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>Job Title</FormLabel>
-              <Input ref={titleRef} placeholder='Job Title' />
-            </FormControl>
+              <ModalBody pb={6}>
+                <FormControl>
+                  <FormLabel>Job Title</FormLabel>
+                  <Input ref={titleRef} placeholder='Job Title' />
+                </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>Company Name</FormLabel>
-              <Input ref={companyRef} placeholder='Company' />
-            </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Company Name</FormLabel>
+                  <Input ref={companyRef} placeholder='Company' />
+                </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>Message</FormLabel>
-              <Input ref={messageRef} placeholder='Message' />
-            </FormControl>
+                <FormControl mt={4}>
+                  <FormLabel>Message</FormLabel>
+                  <Input ref={messageRef} placeholder='Message' />
+                </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>Salary</FormLabel>
-              <Input ref={salaryRef} placeholder='Salary' />
-            </FormControl>
-            <Accordion defaultIndex={[0]} allowMultiple>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex='1' textAlign='left'>
-                      Languages
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                  {/* <Checkbox onChange={getLanguages} value='1' size='sm' colorScheme='green'>
+                <FormControl mt={4}>
+                  <FormLabel>Salary</FormLabel>
+                  <Input ref={salaryRef} placeholder='Salary' />
+                </FormControl>
+                <Accordion defaultIndex={[0]} allowMultiple>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                        <Box as='span' flex='1' textAlign='left'>
+                          Languages
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      {/* <Checkbox onChange={getLanguages} value='1' size='sm' colorScheme='green'>
                     JavaScript
                   </Checkbox>
                   <Checkbox onChange={getLanguages} value='5' size='sm' colorScheme='green'>
@@ -147,11 +165,14 @@ const Feed = () => {
                   <Checkbox onChange={getLanguages} value='10' size='sm' colorScheme='green'>
                     Go
                   </Checkbox> */}
-                  <Languages languages={languages} getLanguages={getLanguages}/>
-                </AccordionPanel>
-              </AccordionItem>
-            </Accordion>
-            {/* <Stack spacing={[1, 5]} direction={['column', 'row']}>
+                      <Languages
+                        languages={languages}
+                        getLanguages={getLanguages}
+                      />
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+                {/* <Stack spacing={[1, 5]} direction={['column', 'row']}>
               <FormLabel>Languages</FormLabel>
               <Checkbox size='sm' colorScheme='green'>
                 JavaScript
@@ -163,17 +184,17 @@ const Feed = () => {
                 React
               </Checkbox>
             </Stack> */}
-            
-          </ModalBody>
+              </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={handleNewPost}>Post</Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-
-        </ModalContent>
-      </Modal>
-      </div>
+              <ModalFooter>
+                <Button colorScheme='blue' mr={3} onClick={handleNewPost}>
+                  Post
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </div>
       </div>
       <div className='post-container'>
         <Post />
@@ -181,6 +202,5 @@ const Feed = () => {
     </div>
   );
 };
-
 
 export default Feed;
