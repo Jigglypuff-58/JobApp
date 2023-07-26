f39b2c4fe8a4fd87fed5ecd02561673f296e260d
Daniel Park <danielpark@Daniels-MacBook-Air.local>
2023-07-25 18:40:33 -0700

    Fixes import statement

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import Feed from './Containers/Feed.jsx';
 import Login from './Containers/LoginItem.jsx';
+import Feed from './Containers/FeedPage.jsx';
 
 const App = () => {
   return (
